7c04aba5f753cf2ca6e70d209d8726d498b1431d
SergioFontan <sergiofontanllamas@gmail.com>
2024-10-10 13:21:31 +0200

    Clase 09-OCT

---
+++ 2024-10-09. Variables/app.js
@@ -0,0 +1,21 @@
+console.log(5);
+
+// creo una variable llamada edad y asigno el valor 14
+let edad = 14; // creo una variable llamada edad y asigno el valor 14
+
+edad = 23;
+edad = 7;
+
+let cosa;
+
+console.log(edad);
+
+const PI = 3.14;
+
+console.log(PI);
+
+/**
+ * Ejercicio 1.
+ * Crea una variable y asigna tu numero de lista.
+ * La variable se llama orden
+ */
